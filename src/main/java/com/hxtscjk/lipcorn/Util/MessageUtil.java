695db59564e194a29ff1b618fa8ee74aecd613bc
hxtscjk17373 <540248302>
2021-04-29 22:43:20 +0800

    feat: 消息类型判断 指令类型与内容分离

---
+++ src/main/java/com/hxtscjk/lipcorn/Util/MessageUtil.java
@@ -0,0 +1,47 @@
+package com.hxtscjk.lipcorn.Util;
+
+public class MessageUtil {
+
+    /**
+     * 检查消息类型
+     * @param message
+     * @return
+     */
+    public int checkMessage(String message) {
+        int blankPosition = message.contains(" ") ? message.indexOf(' ') : -1;
+        //若存在空格且空格不在首位，则认定为可能是实用指令语句，返回类型1
+        if(blankPosition >=1) {
+            return 1;
+        }
+        //若不是视为其他指令，返回类型2
+        else {
+            return 2;
+        }
+    }
+
+    /**
+     * 获取指令类型
+     * @param message
+     * @return
+     */
+    public String getCommandType(String message) {
+        String commandType = new String();
+        //消息中首个空格位置
+        int blankPosition = message.indexOf(' ');
+        commandType = message.substring(0, blankPosition);
+        return commandType;
+    }
+
+    /**
+     * 获取指令内容
+     * @param message
+     * @return
+     */
+    public String getCommandText(String message) {
+        String commandText = new String();
+        //消息中首个空格位置
+        int blankPosition = message.indexOf(' ');
+        commandText = message.substring(blankPosition+1);
+        return commandText;
+    }
+}
--- src/main/java/com/hxtscjk/lipcorn/robot/listen/MessageListener.java
+++ src/main/java/com/hxtscjk/lipcorn/robot/listen/MessageListener.java
@@ -1,18 +1,28 @@
 package com.hxtscjk.lipcorn.robot.listen;
 
+import com.hxtscjk.lipcorn.Util.MessageUtil;
 import love.forte.common.ioc.annotation.Beans;
+import love.forte.simbot.annotation.OnGroup;
 import love.forte.simbot.annotation.OnPrivate;
+import love.forte.simbot.api.message.events.GroupMsg;
 import love.forte.simbot.api.message.events.PrivateMsg;
 import love.forte.simbot.api.sender.MsgSender;
+import love.forte.simbot.api.sender.Sender;
 
 @Beans
 public class MessageListener {
 
-    /**
-     * 私聊消息（用来测试的）
-     * @param privateMsg
-     * @param sender
-     */
+    @OnGroup
+    public void GroupMsg(GroupMsg groupMsg, MsgSender sender) {
+        MessageUtil messageUtil = new MessageUtil();
+        int messageType = messageUtil.checkMessage(groupMsg.getMsg());
+        if(messageType == 1) {
+            String commandType = messageUtil.getCommandType(groupMsg.getMsg());
+            String commandText = messageUtil.getCommandText(groupMsg.getMsg());
+            //测试内容是否已分离出
+            sender.SENDER.sendGroupMsg(groupMsg, commandText);
+        }
+    }
     @OnPrivate
     public void privateMsg(PrivateMsg privateMsg, MsgSender sender) {
         //私聊消息复读
